1ac487fdecf3b1176a4e62d5926364667adb5caf
irqshake <irqshake@proton.me>
2026-05-12 12:13:10 +0530

    service: Fix grammar in a comment

--- service.sh
+++ service.sh
@@ -96,7 +96,7 @@ done
 echo "60" > /proc/sys/vm/stat_interval
 
 # Frequency Governor { Comment this part if your SoC is armv8.5 }
-# Frequency invariant calculations is not supported on Socs less than armv8.5
+# Frequency invariant calculations are not supported on Socs less than armv8.5
 # The cpufreq driver reports a minimum transition latency of 1000us. If 2 cpufreq transitions takes place within this duration then they may cause stale data to schedutil. Therefore, set minimum rate limit to 1ms.
 # It is unlikely to affect performance as very rarely the gap between 2 cpufreq transitions is going to be less than 1 ms.
 
